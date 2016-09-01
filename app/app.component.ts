1c9d65d9524ef4352778266b7777898c558653ce
Gokula2010 <Gokula2010@yahoo.com>
2016-09-01 12:10:07 +0800

    hero-detail compoent added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+
 @Component({
     selector: 'my-app',
     template: 
     `
     <h1>{{title}}</h1>
-    <div *ngIf="selectedHero">
-        <h2>{{selectedHero.name}} details!</h2>
-        <div>
-            <label>id: </label>{{selectedHero.id}}
-        </div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="selectedHero.name" placeholder="name">
-        </div>
-    </div>
+
     <h2>My Heroes</h2>
     <ul class="heroes">
         <li *ngFor="let hero of heroes" [class.selected]="hero===selectedHero" (click)="onSelect(hero)">
@@ -21,6 +14,7 @@ import { Component } from '@angular/core';
         </li>
     </ul>
 
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
 
     `,
     styles: [
@@ -98,10 +92,7 @@ export class AppComponent {
     }
 }
 
-export class Hero {
-    id: number;
-    name: string;
-}
+
 
 
 const HEROES: Hero[] = [
